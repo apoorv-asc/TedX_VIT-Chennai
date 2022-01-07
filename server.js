c427cb7bf8e51d0a38a2ac51bd986141c76c2a4a
apoorv-asc <apoorvsingh.chauhan2019@vitstudent.ac.in>
2022-01-08 02:25:10 +0530

    TedXVITChennai Oriignal

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs");
+app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/tedcircles", (req, res) => {
+  res.render("tedcircles");
+});
+
+app.get("/editions", (req, res) => {
+  res.render("editions");
+});
+
 
 app.listen(5000, process.env.IP, () => {
   console.log("Connected to server");
